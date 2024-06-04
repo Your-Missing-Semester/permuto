af2fe8e02fece1afb3ebadb2030fe4c20e775efc
AnnaY11 <144082695+AnnaY11@users.noreply.github.com>
2024-06-04 11:07:21 -0400

    Changes the login form component from a Class component to a Functional component

--- frontend/src/components/login_page/login.jsx
+++ frontend/src/components/login_page/login.jsx
@@ -1,104 +1,84 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './login.css'
 import Logo_orange_tan from '../assets/logo_orange_tan.svg'
 import Logo_orange from '../assets/logo_orange_black.svg'
 
-class Form extends Component {
-    constructor(props) {
-        super(props);
+const LoginForm = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const loginInfo = {email, password}
+        console.log(loginInfo);
+    }
 
-        this.state = {
-            email: '',
-            password: ''
-        };
-      }
-
-      handleEmailChange = event => {
-        this.setState({
-            email: event.target.value
-        })
-      }
-
-      handlePasswordChange = event => {
-        this.setState({
-            password: event.target.value
-        })
-      }
-
-      handleSubmit = event => {
-        alert('An email and password have been submitted to the login page.'); 
-        event.preventDefault();
-      }
-
-      render(){
-        return (
-            <div className="login-page">
-                <div className='form-side'>
-                    <div className="header">  
-                        <header className="logo">
-                            <img src={Logo_orange} alt="Permuto Logo" id='logo_symbol'/>
-                        </header>
-            
-                        <div className="title">
-                            <h1>SIGN IN</h1>
-                        </div>
-
+    return (
+        <div className="login-page">
+            <div className='form-side'>
+                <div className="header">  
+                    <header className="logo">
+                        <img src={Logo_orange} alt="Permuto Logo" id='logo_symbol'/>
+                    </header>
+        
+                    <div className="title">
+                        <h1>SIGN IN</h1>
                     </div>
 
-                    <form onSubmit={this.handleSubmit}>
-                        <div className='form'>
-                            <label htmlFor="">EMAIL</label><br />
-                            <input 
-                            type="text"
-                            placeholder='Text' 
-                            value={this.state.email} 
-                            onChange={this.handleEmailChange}/><br />
-
-                            <label htmlFor="">PASSWORD</label><br />
-                            <input 
-                            type="text" 
-                            placeholder='Text'
-                            value={this.state.password} 
-                            onChange={this.handlePasswordChange}/><br />
-                        </div>
-
-                        <div className='forgot-password-link'>
-                            <a href="" id='forgot-password-link'>Forgot password?</a>           {/*TODO (T10): replace with correct link*/}
-                        </div>
-
-                        <div className='login-signup-buttons'>
-                            <div className='login-button'>
-                                <button type="submit">LOGIN</button>
-                            </div>
-
-                            <div className='sign-up-link'>
-                                <a href="" id='sign-up-link'>Sign Up</a>                        {/*TODO (T10): replace with correct link*/}
-                            </div>
-                        </div>
-
-                    </form>
-                    
-                    <div className='login-as-header'>
-                        <hr />
-                        <p>LOGIN [AS]</p>
-                    </div>
-                    <div className='login-as-body'>
-                        <p>[placeholder]</p>
-                        <p>[placeholder]</p>
-                        <p>[placeholder]</p>
-                        <p>[placeholder]</p>
-                    </div>
                 </div>
 
-                <div className="image-side">
-                    <img src={Logo_orange_tan} alt="Permuto Skill Exchanging" />
+                <form onSubmit={handleSubmit}>
+                    <div className='form'>
+                        <label htmlFor="">EMAIL</label><br />
+                        <input 
+                        type="text"
+                        placeholder='Text' 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}/><br />
+
+                        <label htmlFor="">PASSWORD</label><br />
+                        <input 
+                        type="text" 
+                        placeholder='Text'
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}/><br />
+                    </div>
+
+                    <div className='forgot-password-link'>
+                        <a href="" id='forgot-password-link'>Forgot password?</a>           {/*TODO (T10): replace with correct link*/}
+                    </div>
+
+                    <div className='login-signup-buttons'>
+                        <div className='login-button'> 
+                            <button>LOGIN</button>
+                        </div>
+
+                        <div className='sign-up-link'>
+                            <a href="" id='sign-up-link'>Sign Up</a>                        {/*TODO (T10): replace with correct link*/}
+                        </div>
+                    </div>
+
+                </form>
+                
+                <div className='login-as-header'>
+                    <hr />
+                    <p>LOGIN [AS]</p>
                 </div>
-    
+                <div className='login-as-body'>
+                    <p>[placeholder]</p>
+                    <p>[placeholder]</p>
+                    <p>[placeholder]</p>
+                    <p>[placeholder]</p>
+                </div>
             </div>
 
-        )
-      }
+            <div className="image-side">
+                <img src={Logo_orange_tan} alt="Permuto Skill Exchanging" />
+            </div>
+
+        </div>
+
+    )
 }
 
 
-export default Form
+export default LoginForm
