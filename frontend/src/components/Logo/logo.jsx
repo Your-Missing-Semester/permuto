fd424ab3a7a5912319889b18dad0000e704de528
suzunak <suzunakimura@gmail.com>
2024-06-12 16:07:18 -0400

    logo.js to .jsx; imported styles; changed .scss to module.css

---
+++ frontend/src/components/Logo/logo.jsx
@@ -0,0 +1,12 @@
+import styles from './logo.module.css';
+
+function Logo() {
+    return (
+        <div className={styles["app-logo"]}>
+            {/* TODO T19: add app logo */}
+            <img src="" alt="App logo" />
+        </div>
+    );
+}
+
+export default Logo;
