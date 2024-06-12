a641b95170f24c971171576f3760aad4a0a32b73
suzunak <suzunakimura@gmail.com>
2024-06-12 16:09:24 -0400

    banner.js to .jsx; imported styles; changed scss to module.css

---
+++ frontend/src/components/Banner/banner.jsx
@@ -0,0 +1,12 @@
+import styles from './banner.module.css';
+
+function Banner() {
+    return (
+        <div className={styles["banner"]}>
+            {/* TODO T18: replace banner / img */}
+            <img src="" alt="Banner" />
+        </div>
+    );
+}
+
+export default Banner;
