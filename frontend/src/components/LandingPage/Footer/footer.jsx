ac31854d6e99d340f721fda64c2f19861c60b5d4
suzunak <suzunakimura@gmail.com>
2024-06-12 19:03:10 -0400

    File js to jsx; scss to module.css; fixed class names

---
+++ frontend/src/components/LandingPage/Footer/footer.jsx
@@ -0,0 +1,81 @@
+import styles from './footer.module.css';
+import { Logo } from '../../index';
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+            <div className={styles["left-container"]}>
+                <header className={styles.logo}>
+                    <Logo />
+                </header>
+
+                <div className={styles.links}>
+                    <a
+                        className={styles["terms-link"]}
+                        // TODO T28: place Terms of Service link
+                        href="replace"
+                    >
+                        Terms of Service|
+                    </a>
+
+                    <a
+                        className={styles["Privacy-link"]}
+                        // TODO T29: place Privacy Policy link 
+                        href="replace"
+                    >
+                        Privacy Policy
+                    </a>    
+                </div>
+
+                <p>
+                    Copyright @ 2020 Permuto | All Right Reserved
+                </p>
+            </div>
+
+            <div className={styles["right-container"]}>
+                <div className={styles.socials}>
+                    <p>
+                        <b>FOLLOW US</b>
+                    </p>
+
+                    <a
+                        className={styles.instagram}
+                        // TODO T30: place instagram link
+                        href="replace"
+                    >
+                        <img src="" alt="instagram logo"></img>
+                    </a>
+
+                    <a
+                        className={styles.facebook}
+                        // TODO T30: place facebook link
+                        href="replace"
+                    >
+                        <img src="" alt="facebook logo"></img>
+                    </a>
+
+                    <a
+                        className={styles.X}
+                        // TODO T30: place X link
+                        href="replace"
+                    >
+                        <img src="" alt="X logo"></img>
+                    </a>
+                </div>
+
+                <div className={styles.emails}>
+                    {/* TODO T31: place correct email */}
+                    <p>
+                        Business Inquiries help@permuto.com
+                    </p>
+
+                    <p>
+                        Customer Supprt help@permuto.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
