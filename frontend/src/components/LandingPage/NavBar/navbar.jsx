2b325f6d93776863ab769e76290a094b319852c1
suzunak <suzunakimura@gmail.com>
2024-06-12 19:30:11 -0400

    Fixed class name and alignment

--- frontend/src/components/LandingPage/NavBar/navbar.jsx
+++ frontend/src/components/LandingPage/NavBar/navbar.jsx
@@ -4,27 +4,30 @@ import { Link } from 'react-router-dom';
 function NavBar() {
     return (
         <div className={styles["nav-bar"]}>
-            <div className={styles["nav-bar-links"]}>
+            <div className={styles["nav-bar-link"]}>
                 {/* TODO: T26: place home link here */}
                 <Link to="/"> 
                     HOME
                 </Link>
-
+            </div>
+            <div className={styles["nav-bar-link"]}>
                 {/* TODO: T11: place sign up link here */}
                 <Link to="/signup">
                     SIGN UP 
                 </Link>
-                
+            </div>
+            <div className={styles["nav-bar-link"]}>
                 {/* TODO: T10: place log in link here */} 
                 < Link to="/login">
                     LOG IN
                 </Link>
-                
+            </div>
+            <div className={styles["nav-bar-link"]}>
                 {/* TODO: T27: place about link here */}
                 < Link to="">
                     ABOUT
-                </Link>   
-            </div>
+                </Link> 
+            </div>  
         </div>
     );
 }
