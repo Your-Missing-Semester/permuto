054098fbbf33cbb66a7bfe69c14ea9e748683480
iamjny <jjng435@gmail.com>
2024-06-16 16:26:22 -0400

    T15: Build the change username form

---
+++ frontend/src/components/ChangeUsername/changeusername.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import styles from './changeusername.module.css'
+
+const ChangeUsername = () => {
+    const [newUsername, setNewUsername] = useState('');
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        alert('Username has been changed!');
+    }
+
+    return (
+    <div className={styles["change-user-page"]}>
+    <div className={styles.header}>
+        <h5>Change username</h5>
+    </div>
+
+    <div className={styles["change-user-form"]}>
+        <form onSubmit={handleSubmit}>
+            <div className={styles["change-input"]}>
+                <label>Enter new username:</label>
+                <input className= {styles["user-input"]}
+                type="text"
+                name="new-user"
+                placeholder="New username"
+                value={newUsername}
+                onChange={(e) => setNewUsername(e.target.value)}
+                required
+                />
+            </div>
+
+            <div className={styles["confirm-button"]}>
+                <button type="submit" className={styles["confirm-btn"]}>CHANGE</button>
+            </div>
+        </form>
+    </div>
+    </div>
+    )
+}
+
+export default ChangeUsername
