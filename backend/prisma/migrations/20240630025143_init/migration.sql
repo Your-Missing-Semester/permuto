12ad784760709075b783b3eb0a684fb14b321d79
iamjny <jjng435@gmail.com>
2024-06-29 22:53:24 -0400

    Changes to prisma.schema and generated new prisma migration

--- backend/prisma/migrations/20240630025143_init/migration.sql
+++ backend/prisma/migrations/20240630025143_init/migration.sql
@@ -3,9 +3,7 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "profileId" TEXT NOT NULL,
-    CONSTRAINT "User_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "password" TEXT NOT NULL
 );
 
 -- CreateTable
@@ -13,7 +11,9 @@ CREATE TABLE "Profile" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "stars" INTEGER NOT NULL,
     "location" TEXT NOT NULL,
-    "description" TEXT NOT NULL
+    "description" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -28,4 +28,4 @@ CREATE TABLE "Skill" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_profileId_key" ON "User"("profileId");
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
