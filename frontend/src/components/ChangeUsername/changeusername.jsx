3cdf59f9a69f5c2bcf16dde72bdfafed6625a670
suzunak <suzunakimura@gmail.com>
2024-07-08 13:38:16 -0400

    Fixed module.css file capitalization

--- frontend/src/components/ChangeUsername/changeusername.jsx
+++ frontend/src/components/ChangeUsername/changeusername.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import styles from './changeusername.module.css'
+import styles from './changeUsername.module.css'
 import axios from 'axios'
 
 const ChangeUsername = () => {
