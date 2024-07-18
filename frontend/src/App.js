a665978bb74df96d8ddae4a189beb3d957c368b3
brigittewylee <brigitte.leewy@gmail.com>
2024-07-18 00:50:46 -0400

    remove unused browser comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import LandingPage from './pages/LandingPage/landingpage';
 import Login from './components/Login/login';
 import Signup from './components/SignUp/Signup';
 import Profile from './components/Profile/Profile';
-import ChangeUsername from './components/ChangeUsername/changeusername';
+import ChangeUsername from './components/ChangeUsername/changeUsername';
 import NavBar from './components/NavBar/navBar';
 import ResetPassword from './components/ResetPassword/resetpassword';
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
