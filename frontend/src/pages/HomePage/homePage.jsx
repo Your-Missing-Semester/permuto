b9f639eddb3da9557b1fbd570310a6798d553d2f
brigittewylee <brigitte.leewy@gmail.com>
2024-07-18 01:52:23 -0400

    home page setup

---
+++ frontend/src/pages/HomePage/homePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from '../../components/HomePage/NavBar/navBar';
+import HomePageBody from '../../components/HomePage/Body/homePageBody';
+
+function HomePage() {
+    return (
+        <div>
+            <NavBar />
+            <HomePageBody />
+        </div>
+    );
+}
+
+export default HomePage;
