3338510412c451c5b01b06b81e60ca2089688215
brigittewylee <brigitte.leewy@gmail.com>
2024-07-23 01:05:22 -0400

    removed console log

--- frontend/src/components/HomePage/Profiles/listOfSkills.jsx
+++ frontend/src/components/HomePage/Profiles/listOfSkills.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import styles from './profiles.module.css';
 
 function LOS ({ user }) {
-    console.log('user prop:', user);
-    if (!Array.isArray(user.skills)) {
-        return (":(");
-    }
     return (
         <ul className={styles["skills"]}>
             {user.skills.map((skill, index) => (
